73f7720473350af0656ddd3ebbf1fc61e9f6c6ca
READMEmaybe <haroune11411@gmail.com>
2023-08-11 02:02:47 +0100

    fix task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     elif len(sys.argv) == 2:
         print("1 argument:")
     else:
-        print("{} arguments:".format(len(sys.argv)))
+        print("{} arguments:".format(len(sys.argv) - 1))
     index = 1
     for arg in sys.argv[1:]:
         print("{}: {}".format(index, arg))
