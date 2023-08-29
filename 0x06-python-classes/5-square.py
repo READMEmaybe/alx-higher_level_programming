511cfffbb73a208368e4306f92c0de51314ab361
READMEmaybe <haroune11411@gmail.com>
2023-08-30 00:20:32 +0100

    add task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """The square"""
+    def __init__(self, size=0):
+        """Initialize Square"""
+        self.size = size
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
