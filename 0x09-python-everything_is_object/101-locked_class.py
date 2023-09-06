0caa099fad81c3274ba72a5aef665c4d7b19d695
READMEmaybe <haroune11411@gmail.com>
2023-09-06 01:30:33 +0100

    fix

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,9 +5,10 @@
 class LockedClass:
     """Defines a locked class.
 
-        The `LockedClass` class is designed to have a limited attribute set specified by
-        the `__slots__` attribute. Instances of this class can only have an attribute named
-        'first_name' and cannot have any additional attributes added dynamically.
+        The `LockedClass` class is designed to have
+        a limited attribute set specified by the `__slots__` attribute.
+        Instances of this class can only have an attribute named 'first_name'
+        and cannot have any additional attributes added dynamically.
 
     Attributes:
         first_name (str): A string representing the first name of an object.
