51abb3f3fd0712008d6bf7b1286641456dff89d7
READMEmaybe <haroune11411@gmail.com>
2023-09-06 01:22:53 +0100

    added 0x09-python-everything_is_object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a locked class."""
+
+
+class LockedClass:
+    """Defines a locked class.
+
+        The `LockedClass` class is designed to have a limited attribute set specified by
+        the `__slots__` attribute. Instances of this class can only have an attribute named
+        'first_name' and cannot have any additional attributes added dynamically.
+
+    Attributes:
+        first_name (str): A string representing the first name of an object.
+    """
+    __slots__ = ["first_name"]
