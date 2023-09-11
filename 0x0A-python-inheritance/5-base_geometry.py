b20a0a778963487a327a4f76643e159c9075af9d
READMEmaybe <haroune11411@gmail.com>
2023-09-11 21:36:42 +0100

    add task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines a base class, BaseGeometry, for geometric objects."""
+
+
+class BaseGeometry:
+    """
+    A base class for geometric objects.
+
+    This class serves as the foundation for defining various geometric objects
+    and their properties.
+    """
+    pass
