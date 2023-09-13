fe35fa4d899bf5ef4f964663a04794fcab1ee705
READMEmaybe <haroune11411@gmail.com>
2023-09-13 01:13:17 +0100

    add task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -14,8 +14,10 @@ class Student:
         last_name (str): The last name of the student.
         age (int): The age of the student.
     Methods:
-        to_json(self): Converts the student object to a
-        JSON-serializable dictionary.
+         to_json(self, attrs=None): Converts the student object to a
+         JSON-serializable dictionary.
+         reload_from_json(self, json): Updates the student object's
+         attributes from a JSON dictionary.
     """
     def __init__(self, first_name, last_name, age):
         """
@@ -30,11 +32,27 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """
         Converts the student object to a JSON-serializable dictionary.
 
+        Args:
+            attrs (list, optional): A list of attribute names to include in
+            the JSON representation. Defaults to None.
         Returns:
             dict: A dictionary containing the student's information.
         """
+        if (isinstance(attrs, list) and
+                all(isinstance(ele, str) for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Updates the student object's attributes from a JSON dictionary.
+
+        Args:
+            json (dict): A dictionary containing attribute-value pairs.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
