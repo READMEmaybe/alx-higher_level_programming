ccfbd46f332a7a8a3f49526d928332b05460f046
READMEmaybe <haroune11411@gmail.com>
2023-09-13 01:08:48 +0100

    add task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""This module defines a Student class for representing student information."""
+
+
+class Student:
+    """
+    A class representing a student.
+
+    This class stores information about a student, including their first name,
+    last name, and age.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    Methods:
+         to_json(self, attrs=None): Converts the student object to a
+         JSON-serializable dictionary.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with the provided information.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Converts the student object to a JSON-serializable dictionary.
+
+        Args:
+            attrs (list, optional): A list of attribute names to include in
+            the JSON representation. Defaults to None.
+        Returns:
+            dict: A dictionary containing the student's information.
+        """
+        if (isinstance(attrs, list) and
+                all(isinstance(ele, str) for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
