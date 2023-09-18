88c83f1c275c81b4377f054e3e31e234bcd22337
READMEmaybe <haroune11411@gmail.com>
2023-09-19 00:15:31 +0100

    revert

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,15 +50,14 @@ class Base:
         Args:
             list_dictionaries (list): A list of dictionaries to
             be converted to JSON.
-
         Returns:
             str: The JSON-formatted string representing the input
             list of dictionaries.
         """
 
-        if list_dictionaries:
-            return json.dumps(list_dictionaries)
-        return "[]"
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
